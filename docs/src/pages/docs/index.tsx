258af50464b8cbc4dd9237ce9fc597d372d84a29
gcarwardine <glenn.carwardine@zepben.com>
2021-01-25 14:38:44 +1100

    Update descriptions of doc components (#17)
    * Update descriptions of doc components
    
    Signed-off-by: Glenn Carwardine <glenn.carwardine@zepben.com>
    
    * Fix case for energy workbench
    
    Signed-off-by: Varun Ramani <varun.ramani@zepben.com>
    
    Co-authored-by: Varun Ramani <varun.ramani@zepben.com>

--- docs/src/pages/docs/index.tsx
+++ docs/src/pages/docs/index.tsx
@@ -8,17 +8,19 @@ const Ui: FunctionComponent = () => {
             description={"Documentation for components of the Evolve platform"}
             items={[
                 {
-                    title: "Evolve JVM",
+                    title: "Evolve JVM SDK",
                     description:
-                        "The JVM SDK to communicate with the Energy Workbench Server",
+                        "Java / Kotlin JVM implementation of the Evolve CIM profile and related tools for working with data models. " +
+                        "Also contains interfaces for communicating with the Energy Workbench Network Server.", 
                     siteRelUrl: "jvm-sdk",
                     repoUrl: "https://github.com/zepben/evolve-sdk-jvm",
                     type: "SDKs",
                 },
                 {
-                    title: "Evolve Python",
+                    title: "Evolve Python SDK",
                     description:
-                        "Python SDK to communicate with the Energy Workbench Server",
+                        "Python implementation of the Evolve CIM profile and related tools for working with data models. " +
+                        "Also contains interfaces for communicating with the Energy Workbench Network Server.", 
                     siteRelUrl: "python-sdk",
                     repoUrl: "https://github.com/zepben/evolve-sdk-python",
                     type: "SDKs",
@@ -26,7 +28,7 @@ const Ui: FunctionComponent = () => {
                 {
                     title: "Energy Workbench Server",
                     description:
-                        "Data platform for energy networks based on the Common Information Model. ",
+                        "Data server for energy networks built on top of the Evolve SDK.",
                     repoUrl: "https://github.com/zepben/energy-workbench-server",
                     siteRelUrl: "energy-workbench-server",
                     type: "Servers",
@@ -34,7 +36,7 @@ const Ui: FunctionComponent = () => {
                 {
                     title: "Energy Network Server",
                     description:
-                        "Headless network server",
+                        "Cut down version of the Energy Workbench server to provide only network models and related functions.",
                     repoUrl: "https://github.com/zepben/ewb-network-server",
                     siteRelUrl: "network-server",
                     type: "Servers",
@@ -42,91 +44,91 @@ const Ui: FunctionComponent = () => {
                 {
                     title: "PoF Historian Server",
                     description:
-                        "Provides access to PoF Historian data",
+                        "REST-like API access to PowerOn Fusion historical analog data.",
                     siteRelUrl: "pof-historian",
                     type: "Servers",
                 },
                 {
                     title: "Energy Workbench Web Client",
                     description:
-                        "UI Client to view load profiles, load analysis and geographic view of the network",
+                        "Web app to view load profiles, load analysis and geographic views of the network hosted by the Energy Workbench.",
                     siteRelUrl: "ewb-web-client",
                     type: "User Interface",
                 },
                 {
                     title: "PoF Network Extractor",
                     description:
-                        "PoF DMS adapter to extract network into a format consumable by the Energy Workbench Server",
+                        "PowerOn Fusion DMS adapter to extract network models into the Energy Workbench Server format.",
                     siteRelUrl: "pof-network-extractor",
                     type: "Migrators",
                 },
                 {
                     title: "GIS Network Extractor",
                     description:
-                        "GIS adapter to extract network into a format consumable by the Energy Workbench Server",
+                        "GIS adapter to extract network models into the Energy Workbench Server format.",
                     siteRelUrl: "gis-network-extractor",
                     type: "Migrators",
                 },
                 {
                     title: "Load Aggregator",
                     description:
-                        "Data processor to perform aggregations of energy usage to distribution transformers",
+                        "Data processor to perform aggregations of energy usage to distribution transformers.",
                     siteRelUrl: "load-aggregator",
                     type: "Data Processors",
                 },
                 {
                     title: "Load Processor",
                     description:
-                        "Data processor to process meter energy usage and create a time series database",
+                        "Data processor to process CSV meter energy usage records and create a time series database.",
                     siteRelUrl: "load-processor",
                     type: "Data Processors",
                 },
                 {
                     title: "Weather Processor",
                     description:
-                        "Data processor to process weather data and create a database",
+                        "Data processor to process CSV weather data and create a database.",
                     siteRelUrl: "weather-processor",
                     type: "Data Processors",
                 },
                 {
                     title: "Energy Workbench Bundle",
                     description:
-                        "Bundle that contains all the tools needed to setup a workbench",
+                        "A bundle that contains all the tools with compatible versions needed to setup a workbench.",
                     siteRelUrl: "ewb-bundle",
                     type: "Bundles"
                 },
                 {
                     title: "Evolve Profile",
                     description:
-                        "Evolve profile of the CIM data model",
+                        "Evolve profile of the CIM data model.",
                     siteRelUrl: "cim/evolve",
                     type: "Data Models",
                 },
                 {
                     title: "CIM100 Data Model",
                     description:
-                        "CIM100 data model",
+                        "CIM100 data model.",
                     siteRelUrl: "cim/cim100",
                     type: "Data Models"
                 },
                 {
                     title: "Feeder Load Analysis",
                     description:
-                        "Runs load analysis on feeder segments",
+                        "Runs load analysis of feeder segments on network models provided by the Energy Workbench.",
                     siteRelUrl: "feeder-load-analysis",
                     type: "Tools"
                 },
                 {
                     title: "SINCAL Exporter",
                     description:
-                        "Exporter to create SINCAL models using the network from DMS or GIS",
+                        "Exporter to create SINCAL models from the network models provided by the Energy Workbench.",
                     siteRelUrl: "sincal-exporter",
                     type: "SINCAL"
                 },
                 {
-                    title: "Smart Load Plugin",
+                    title: "SINCAL Smart Load Plugin",
                     description:
-                        "SINCAL plugin to access load data from energy workbench for running load flow studies",
+                        "SINCAL plugin that provides load data from the Energy Workbench when running load flow studies.",
                     siteRelUrl: "smart-load-plugin",
                     type: "SINCAL"
                 },
